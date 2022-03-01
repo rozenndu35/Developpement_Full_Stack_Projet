e285838d7321e4c03cbc78919e4a88bc6aef06c8
Costiou Rozenn <rozenn.costiou@orange.fr>
2022-03-01 19:04:11 +0100

    Feat : creation du header

---
+++ Frontend/project-react/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import Navigation from '../Navigation/Navigation';
+
+
+export default function Header(props) {
+    return (
+        <header className="App-header">
+            <h1 className="App-title">Delta - Blog - Projet Hilleriteau Costiou</h1>            
+        </header>
+    )
+}
