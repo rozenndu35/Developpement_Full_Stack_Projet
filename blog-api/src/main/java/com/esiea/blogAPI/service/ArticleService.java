5d19428f2f98b873f1ab9c67c32eae54e5381f1f
bhilleri <benjaminh@live.fr>
2022-03-04 00:15:17 +0100

    Feat : ajout de getArticle

--- blog-api/src/main/java/com/esiea/blogAPI/service/ArticleService.java
+++ blog-api/src/main/java/com/esiea/blogAPI/service/ArticleService.java
@@ -1,5 +1,17 @@
 package com.esiea.blogAPI.service;
 
-public class ArticleService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.esiea.blogAPI.model.Article;
+import com.esiea.blogAPI.repository.ArticleRepository;
+
+@Service
+public class ArticleService {
+	@Autowired
+	private ArticleRepository articleRepository;
+	
+	public Iterable<Article> getArticle(){
+		return articleRepository.findAll();
+	}
 }
