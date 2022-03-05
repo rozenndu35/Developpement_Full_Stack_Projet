68731434fb55691899a3e68abd421a8e09eb1993
Costiou Rozenn <rozenn.costiou@orange.fr>
2022-03-05 22:57:18 +0100

    Feat : refacto non fonctionnele mais pue de batterie sur pc et toujours pas d'electricite

--- Frontend/project-react/src/App.js
+++ Frontend/project-react/src/App.js
@@ -4,81 +4,351 @@ import Header from './components/Header/Header';
 import Body from './components/Body/Body';
 import Footer from './components/Footer/Footer';
 
-function App() {
-  const allCategory = [{
-    id: 0,
-    name: "Vetement"
-  }, {
-    id: 1,
-    name: "Numerique"
-  },{
-    id: 2,
-    name: "Meuble"
-  },{
-    id: 10,
-    name: "Meuble"
-  }]
-  const allArticle =[ {
-    "id": 1,
-    "title": "categoryName1",
-    "publicationDate": "2021-12-31T23:00:00.000+00:00",
-    "content": "article1",
-    "author": {
-        "id": 1,
-        "firstName": "firstName1",
-        "lastName": "lastName1"
-    }
-  },
-  {
-      "id": 2,
-      "title": "categoryName2",
-      "publicationDate": "2022-02-01T23:00:00.000+00:00",
-      "content": "article2",
-      "author": {
-          "id": 2,
-          "firstName": "firstName2",
-          "lastName": "lastName2"
-      }
-  }]
+import { useEffect, useState } from 'react';
 
-  const articleUsing =[
+function App() {
+
+  const [postingCategory, setPostingCategory] = useState(false);
+  const [toDeleteCategory, setToDeleteCategory] = useState(false);
+  const [postingArticle, setPostingArticle] = useState(false);
+  const [toDeleteArticle, setToDeleteArticle] = useState(false);
+  const [allCategory, setAllCategory] = useState(
+    [{
+      id: 0,
+      name: "Vetement"
+    }, {
+      id: 1,
+      name: "Numerique"
+    },{
+      id: 2,
+      name: "Meuble"
+    },{
+      id: 10,
+      name: "Meuble"
+    }]
+  );
+  const [allArticle, setAllArticle] = useState(
+    [ {
+      "id": 1,
+      "title": "categoryName1",
+      "publicationDate": "2021-12-31T23:00:00.000+00:00",
+      "content": "article1",
+      "author": {
+          "id": 1,
+          "firstName": "firstName1",
+          "lastName": "lastName1"
+      }
+    },
     {
-      "id": 2,
-      "title": "categoryName2",
+        "id": 2,
+        "title": "categoryName2",
+        "publicationDate": "2022-02-01T23:00:00.000+00:00",
+        "content": "article2",
+        "author": {
+            "id": 2,
+            "firstName": "firstName2",
+            "lastName": "lastName2"
+        }
+    }]
+  );
+  const [getCategory, setGetCategory] = useState(false);
+  const [getArticle, setGetArticle] = useState(false);
+  const [articleInCategory, setArticleInCategory] = useState(
+    [
+      {
+        "id": 2,
+        "title": "categoryName2",
+        "publicationDate": "2022-02-01T23:00:00.000+00:00",
+        "content": "article2",
+        "author": {
+            "id": 2,
+            "firstName": "firstName2",
+            "lastName": "lastName2"
+        }
+    },{
+      "id": 3,
+      "title": "categoryName3",
       "publicationDate": "2022-02-01T23:00:00.000+00:00",
-      "content": "article2",
+      "content": "article3",
       "author": {
           "id": 2,
           "firstName": "firstName2",
           "lastName": "lastName2"
       }
-  },{
-    "id": 3,
-    "title": "categoryName3",
-    "publicationDate": "2022-02-01T23:00:00.000+00:00",
-    "content": "article3",
-    "author": {
-        "id": 2,
-        "firstName": "firstName2",
-        "lastName": "lastName2"
-    }
-}
-  ]
-  const article ={
-    "id": 1,
-    "title": "categoryName1",
-    "publicationDate": "2021-12-31T23:00:00.000+00:00",
-    "content": "article1",
-    "author": {
-        "id": 1,
-        "firstName": "firstName1",
-        "lastName": "lastName1"
-    }
   }
+    ]
+  );
+  const [article, setArticle] = useState(
+    {
+      "id": 1,
+      "title": "categoryName1",
+      "publicationDate": "2021-12-31T23:00:00.000+00:00",
+      "content": "article1",
+      "author": {
+          "id": 1,
+          "firstName": "firstName1",
+          "lastName": "lastName1"
+      }
+    }
+  );
+
+  const [newCategory, setNewCategory] = useState({
+    id: 0,
+    name: ""
+  });
+  const [newArticle, setNewArticle] = useState({
+      id: 0,
+      name: "",
+      author:"",
+      datetime: new Date(),
+      title: "",
+      content: "",
+      category: ""
+
+  });
+
+  const [inputInvalid, setInputInvalid] = useState(false);
+
+
+  const [CategoryChoice, setCategoryChoice] = useState();
+  const [articleChoice, setArticleChoice] = useState();
+  /*
+  Recupere toutes les Categorys 
+  */
+  useEffect(() => {
+    /*fetch('')
+    .then(res => res.json())
+    .then(data => {
+      setAllCategory(data);
+    })
+    .catch(e => console.log(e.toString()));
+    */
+  }, [postingCategory, toDeleteCategory]);
+
+  /*
+  Recupere toutes les articles 
+  */
+  useEffect(() => {
+    /*fetch('')
+    .then(res => res.json())
+    .then(data => {
+      setAllArticle(data);
+    })
+    .catch(e => console.log(e.toString()));
+    */
+  }, [postingArticle, toDeleteArticle]);
+
+  /*
+  Recuper les articles de la Category where Category = CategoryChoice
+  */
+  useEffect(() => {
+    /*fetch('')
+    .then(res => res.json())
+    .then(data => {
+      setArticleInCategory(data);
+    })
+    .catch(e => console.log(e.toString()));
+    */
+  }, [getCategory]);
+
+  /*
+  Recuper l'articles saisie where article = articleChoice
+  */
+  useEffect(() => {
+    /*fetch('')
+    .then(res => res.json())
+    .then(data => {
+      setArticle(data);
+    })
+    .catch(e => console.log(e.toString()));
+    */
+  }, [getArticle]);
+
+
+  /*
+  Envoie l'ajout de la Category
+  */
+  useEffect(() =>{
+    if (postingCategory) {
+      
+      /*
+      fetch('', {
+          method: "POST",
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(newArticle)
+      })
+      .then(res => res.json())
+      .then(data => {
+        setPostingArticle(false);
+        setNewCategory(prevState => {
+          initInvalidInput();
+
+          return {...prevState,
+              id: 0,
+              name: ""
+          }
+          });
+      })
+      .catch(e => console.log(e.toString()));
+      */
+      allCategory.push(newCategory)
+      setPostingCategory(false);
+      setNewCategory(prevState => {
+          initInvalidInput();
+
+          return {...prevState,
+              id: 0,
+              name: ""
+          }
+          });
+    }
+  }, [postingCategory]);
+
+    /*
+  Envoie l'ajout de l'article
+  */
+  useEffect(() =>{
+    if (postingArticle) {
+      
+      /*
+      fetch('', {
+          method: "POST",
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(newCategory)
+      })
+      .then(res => res.json())
+      .then(data => {
+        setPostingCategory(false);
+        setNewArticle(prevState => {
+        initInvalidInput();
+        return {...prevState,
+          id: 0,
+          name: "",
+          author:"",
+          datetime: new Date(),
+          title: "",
+          content: "",
+          category: ""
+        }
+      });
+      })
+      .catch(e => console.log(e.toString()));
+      */
+      allArticle.push(newArticle)
+      setPostingArticle(false);
+      setNewArticle(prevState => {
+        initInvalidInput();
+        return {...prevState,
+          id: 0,
+          name: "",
+          author:"",
+          datetime: new Date(),
+          title: "",
+          content: "",
+          category: ""
+        }
+      });
+    }
+  }, [postingArticle]);
+
+  /*
+  Remet l'input a valide innitiale
+  */
+  function initInvalidInput() {
+    setInputInvalid(false);
+  }
+  /*
+  valide l'envoie de la Category
+  */
+  function submitCategory() {
+    setPostingCategory(true);
+  }
+  /*
+    valide l'envoie de l'article
+  */
+  function submitArticle() {
+      setPostingArticle(true);
+  }
+
+  /*
+    Modifie la nouvelle Category
+  */
+  function newCategoryChange(event) {
+    const {name, value} = event.target;
+    isString(value) ?
+        setInputInvalid("Vous ne pouvez pas inserer de caractere speciaux") 
+        :
+        value.length > 255 ?
+            setInputInvalid("Le nombre maximum est de 255 caractere")
+            :
+            setNewCategory(prevState => {
+              initInvalidInput();
+
+              return {...prevState,
+                  id: allCategory.length + 1,
+                  [name]: value
+              }
+            });
+  }
+
+  /*
+    Modifie le nouvelle article
+  */
+  function newArticleChange(event) {
+    const {type, name, value} = event.target;
+    name === 'category'?
+        setNewArticle(prevState => {
+            initInvalidInput();
+
+            return {...prevState,
+                id: allArticle.length + 1,
+                [name]: value
+            }
+        })
+    :
+    type === 'text' ?
+
+    isString(value) ?
+        setInputInvalid("Vous ne pouvez pas inserer de caractere speciaux") 
+        :value.length > 255 ?
+            setInputInvalid("Le nombre maximum est de 255 caractere")
+            :
+            setNewArticle(prevState => {
+            initInvalidInput();
+
+            return {...prevState,
+                id: allArticle.length + 1,
+                [name]: value
+            }
+            })
+        
+    :
+    setInputInvalid("Type non texte") 
+  }
+
+  /*
+    Verifie si c'est un string et non pas caractere correspondant un du code potentiel
+  */
+  function isString( value){
+    return value.match(/^.*[<>/\\].*$/);
+}
+
   return (
     <div className="App">
       <Header />
-      <Body allCategory={allCategory} allArticle={allArticle} articlesInCategory={articleUsing} article={article}/>
+      <Body 
+        allCategory={allCategory} articlesInCategory={articleInCategory}
+        allArticle={allArticle} article={article}
+        newCategory={newCategory} submitCategory={submitCategory} handlerCategory={newCategory}
+        newArticle={newArticle} submitArticle={submitArticle} handlerArticle={newArticle}
+        setCategoryChoice={setCategoryChoice}
+      />
       <Footer />
     </div>
   );
--- Frontend/project-react/src/components/Body/Body.js
+++ Frontend/project-react/src/components/Body/Body.js
@@ -10,91 +10,8 @@ import './formulaire.css'
 
 
 export default function Body(props) {
-    const [pageTwo, setPage] = useState();
+    const [page, setPage] = useState();
     const [pageUpdate, setPageUpdate] = useState({update: false, pageId: -1 });
-    const [inputInvalid, setInputInvalid] = useState(false);
-    const [postingCategory, setPostingCategory] = useState(false);
-    const [postingArticle, setPostingArticle] = useState(false);
-    const [categorieChoice, setCategorieChoice] = useState();
-
-    const [newCategory, setNewCategory] = useState({
-        id: 0,
-        name: ""
-      });
-    const [newArticle, setNewArticle] = useState({
-        id: 0,
-        name: "",
-        author:"",
-        datetime: new Date(),
-        title: "",
-        content: "",
-        category: ""
-
-    });
-    function isNumber( value){
-        return value.match(/^[0-9]+$/);
-    }
-    function isString( value){
-        return value.match(/^.*[<>/\\].*$/);
-    }
-    
-
-    function newCategoryChange(event) {
-        const {name, value} = event.target;
-        console.log("___________new change : ");
-        console.log(value);
-        isString(value) ?
-            setInputInvalid("Vous ne pouvez pas inserer de caractere speciaux") 
-            :
-            value.length > 255 ?
-                setInputInvalid("Le nombre maximum est de 255 caractere")
-                :
-                setNewCategory(prevState => {
-                initInvalidInput();
-
-                return {...prevState,
-                    id: props.allCategory.length + 1,
-                    [name]: value
-                }
-                });
-    }
-    
-    function newArticleChange(event) {
-        console.log("----Event---")
-        console.log(event)
-        console.log("----Event---")
-        const {type, name, value} = event.target;
-        console.log("___________new change : ");
-        console.log(value);
-        name === 'category'?
-            setNewArticle(prevState => {
-                initInvalidInput();
-
-                return {...prevState,
-                    id: props.allArticle.length + 1,
-                    [name]: value
-                }
-            })
-        :
-        type === 'text' ?
-    
-        isString(value) ?
-            setInputInvalid("Vous ne pouvez pas inserer de caractere speciaux") 
-            :value.length > 255 ?
-                setInputInvalid("Le nombre maximum est de 255 caractere")
-                :
-                setNewArticle(prevState => {
-                initInvalidInput();
-
-                return {...prevState,
-                    id: props.allArticle.length + 1,
-                    [name]: value
-                }
-                })
-            
-        :
-        setInputInvalid("Type non texte") 
-    }
 
     useEffect(() => {
         if (pageUpdate.update) {
@@ -104,72 +21,22 @@ export default function Body(props) {
         }
     }, [pageUpdate]);
 
-    useEffect(() =>{
-        if (postingCategory) {
-            console.log("ajouter categories : ")
-            console.log(newCategory)
-            props.allCategory.push(newCategory)
-            setPostingCategory(false);
-            setNewCategory(prevState => {
-                initInvalidInput();
-
-                return {...prevState,
-                    id: 0,
-                    name: ""
-                }
-                });
-        }
-    }, [postingCategory]);
-    
-    useEffect(() =>{
-        if (postingArticle) {
-            console.log("ajouter article : ")
-            console.log(newArticle)
-            props.allArticle.push(newArticle)
-            setPostingArticle(false);
-            setNewArticle(prevState => {
-                initInvalidInput();
-                return {...prevState,
-                    id: 0,
-                    name: "",
-                    author:"",
-                    datetime: new Date(),
-                    title: "",
-                    content: "",
-                    category: ""
-                }
-                });
-            console.log(props.allArticle)
-        }
-    }, [postingArticle]);
-
     function afficherPage(event, id, category) {
         event.stopPropagation();
         setPageUpdate({update: true, pageId: id });
-        setCategorieChoice(category)
+        props.setCategoryChoice(category)
     }  
     
-    function initInvalidInput() {
-        setInputInvalid(false);
-    }
-
-    function submitCategory() {
-        setPostingCategory(true);
-    }
-
-    function submitArticle() {
-        setPostingArticle(true);
-    }
-
+    
     return (
         <div className='App-body'>
             <Navigation categories={props.allCategory} afficherPage={afficherPage}/>
             <section className='App-page'>
-                { pageTwo === "ListeCategory" && <ListCategory categories={props.allCategory}  afficherPage={afficherPage}/>}
-                { pageTwo === "ArticleCategory" && <ListArticleInCategorie categorie={categorieChoice} articles={props.articlesInCategory} afficherPage={afficherPage}/>}
-                { pageTwo === "AddCategory" && <AddCategory newCategory={newCategory} inputInvalid={inputInvalid} handleChange={newCategoryChange} submitCategory={submitCategory}/>}
-                { pageTwo === "AddArticle" && <AddArticle newArticle={newArticle} categories={props.allCategory} inputInvalid={inputInvalid} handleChange={newArticleChange} submitArticle={submitArticle}/>}
-                { pageTwo === "Article" && <Article article={props.article} />}
+                { page === "ListeCategory" && <ListCategory categories={props.allCategory}  afficherPage={afficherPage}/>}
+                { page === "ArticleCategory" && <ListArticleInCategorie categorie={props.CategoryChoice} articles={props.articlesInCategory} afficherPage={afficherPage}/>}
+                { page === "AddCategory" && <AddCategory newCategory={props.newCategory} inputInvalid={props.inputInvalid} handleChange={props.newCategoryChange} submitCategory={props.submitCategory}/>}
+                { page === "AddArticle" && <AddArticle newArticle={props.newArticle} categories={props.allCategory} inputInvalid={props.inputInvalid} handleChange={props.newArticleChange} submitArticle={props.submitArticle}/>}
+                { page === "Article" && <Article article={props.article} />}
             </section>
 
         </div>
