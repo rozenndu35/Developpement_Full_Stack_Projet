e020c6eb281ba947c09dbce0fdb0feee913575de
Costiou Rozenn <rozenn.costiou@orange.fr>
2022-03-09 19:33:56 +0100

    Feat : test du composant article

--- Frontend/project-react/src/components/Article/Article.test.js
+++ Frontend/project-react/src/components/Article/Article.test.js
@@ -1,2 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import Article from './Article';
 
-it ("TODO",() => {});
+const articleMoc ={
+    "id": 2,
+    "title": "title2",
+    "publicationDate": "2022-02-01T23:00:00.000+00:00",
+    "content": "article2",
+    "author": {
+        "id": 2,
+        "firstName": "firstName2",
+        "lastName": "lastName2"
+    },
+    "category": {
+        "id": 2,
+        "categoryName": "categoryName2"
+    }
+}
+it ("should Affichage de l'article",() => {
+    render(<Article article={articleMoc}/>);
+    expect(screen.getByText("Article : title2")).toBeTruthy();
+    expect(screen.getByText("firstName2 lastName2 - 2022-02-01T23:00:00.000+00:00")).toBeTruthy();
+    expect(screen.getByText("article2")).toBeTruthy();
+});
