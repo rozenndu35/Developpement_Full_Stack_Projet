17d298030f23e0dd75af1eb7f4206eca1cc75e37
bhilleri <benjaminh@live.fr>
2022-03-17 17:08:57 +0100

    Fix : Renvoie d'un seul auteur dans getByName

--- blog-api/src/main/resources/schema.sql
+++ blog-api/src/main/resources/schema.sql
@@ -3,18 +3,18 @@ DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS author;
 Create TABLE author(
   id_author BIGINT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(250) NOT NULL,
-  last_name VARCHAR(250) NOT NULL
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL
 );
 CREATE TABLE category (
   id_category BIGINT AUTO_INCREMENT PRIMARY KEY,
-  category_name VARCHAR(250) NOT NULL
+  category_name VARCHAR(255) NOT NULL
 );
 CREATE TABLE article (
   id_article BIGINT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(250) NOT NULL,
+  title VARCHAR(255) NOT NULL,
   publication_date TIMESTAMP NOT NULL,
-  content TEXT(1000),
+  content TEXT(10000),
   id_author BIGINT NOT NULL,
   id_category BIGINT NOT NULL
 );
