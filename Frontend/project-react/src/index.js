738ad61ad37cf7aad39481960aefe6b3aa87febd
bhilleri <benjaminh@live.fr>
2022-03-18 13:33:24 +0100

    Fix : Retrait de la dépendance obsolète  Router

--- Frontend/project-react/src/index.js
+++ Frontend/project-react/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Router from './components/Router/Router';
 
 ReactDOM.render(
   <React.StrictMode>
