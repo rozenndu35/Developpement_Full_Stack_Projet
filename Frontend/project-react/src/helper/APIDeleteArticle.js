73fd21703f2f55a0919c347d8f0cd36d9c7800e6
Costiou Rozenn <rozenn.costiou@orange.fr>
2022-03-19 21:33:03 +0100

    Fix : ajout des condition sur la page delete article

--- Frontend/project-react/src/helper/APIDeleteArticle.js
+++ Frontend/project-react/src/helper/APIDeleteArticle.js
@@ -1,8 +1,8 @@
 import ApiConfig from "../config/ApiConfig";
 
 export default function APIDeleteArticle(id){
+    const t = sessionStorage.getItem('token') || "";
     return fetch(ApiConfig.adress + 'private/article/'+ id,{
-        const t = sessionStorage.getItem('token') || "";
         method: "DELETE",
         headers: {
           'Content-Type':'application/json',
