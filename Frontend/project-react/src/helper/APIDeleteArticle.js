bcee292882af0df0ff6c8abb052e643a0d8016f5
Costiou Rozenn <rozenn.costiou@orange.fr>
2022-03-19 21:32:03 +0100

    Feat : ajout des condition sur les autre pages

--- Frontend/project-react/src/helper/APIDeleteArticle.js
+++ Frontend/project-react/src/helper/APIDeleteArticle.js
@@ -2,8 +2,10 @@ import ApiConfig from "../config/ApiConfig";
 
 export default function APIDeleteArticle(id){
     return fetch(ApiConfig.adress + 'private/article/'+ id,{
+        const t = sessionStorage.getItem('token') || "";
         method: "DELETE",
         headers: {
           'Content-Type':'application/json',
+          'Authorization': `Bearer ${t}`,
     },})
 }
