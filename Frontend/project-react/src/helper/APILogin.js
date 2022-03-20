7ff5e8bc04665c63421ae2c7a6e3d145b5cbfc3b
Costiou Rozenn <rozenn.costiou@orange.fr>
2022-03-20 11:47:43 +0100

    Feat : gestion des erreurs avec le serveur pour l'inscription et le login

--- Frontend/project-react/src/helper/APILogin.js
+++ Frontend/project-react/src/helper/APILogin.js
@@ -4,7 +4,15 @@ export default function getLogin(user){
     return fetch(ApiConfig.adress + "public/login",{
         method: 'POST',
         headers: {
+            'Accept': 'text/plain',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(user)})
+
+        .then(res =>{
+            if(res.status === 200){
+               return res.json()
+            }else{
+                return res.status;
+            } })
 }
