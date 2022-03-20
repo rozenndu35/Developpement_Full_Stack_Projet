25993add84c16f5da694f9c732600b244be709cf
Costiou Rozenn <rozenn.costiou@orange.fr>
2022-03-20 08:34:59 +0100

    Feat : modification des url public

--- Frontend/project-react/src/helper/getAuthorByName.js
+++ Frontend/project-react/src/helper/getAuthorByName.js
@@ -1,5 +1,5 @@
 import ApiConfig from "../config/ApiConfig";
 
 export default function getAuthorByName(lastName, firstName){
-    return fetch(ApiConfig.adress + "private/author/?lastName=" + lastName + "&firstName=" + firstName)
+    return fetch(ApiConfig.adress + "public/author/?lastName=" + lastName + "&firstName=" + firstName)
 }
